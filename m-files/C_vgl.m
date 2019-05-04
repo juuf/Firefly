cb7c67ccd1b13a84d8910e05b6077168846fdd1e
juuf <47811956+juuf@users.noreply.github.com>
2019-05-04 15:12:02 +0200

    Update C_vgl.m

--- m-files/C_vgl.m
+++ m-files/C_vgl.m
@@ -1,18 +1,18 @@
 %%
-c_phi_ext = table2array(importfile('./phi_ext.txt'));
+c_phi_ext = table2array(importfile('./phi_ext.txt')); % adapt path to c-files
 
 figure
 plot(c_phi_ext); hold on
 plot(phi_ext,"--")
 %%
 figure
-c_phi = table2array(importfile('./phi.txt'));
+c_phi = table2array(importfile('./phi.txt')); % adapt path to c-files
 plot(c_phi);hold on
 plot(phase,"--");
 
 %%
 figure
-c_omegas = table2array(importfile('./omegas.txt'));
+c_omegas = table2array(importfile('./omegas.txt')); % adapt path to c-files
 plot(c_omegas);hold on
 plot(omegas,"--");
 %%
